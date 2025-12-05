4d26d5d9e32cce113c9572b6485c2382c020ee88
Tory L <lysiktory@gmail.com>
2025-12-05 11:32:41 -0500

    configure for GitHub Pages deployment
    Updated package configuration and vite config for  gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/2025_Tow_university-response-tracker-/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
