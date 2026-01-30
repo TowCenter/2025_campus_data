610a193ce91fd4bf947e650dbbddec1e8b8b8a1c
Tory L <lysiktory@gmail.com>
2026-01-30 11:27:40 -0500

    fix attempted vibe coding fix (this is why i dont vibe code)
    @shua53 fix attempted vibe coding fix (this is why i dont vibe code)

--- dist/Article.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	let { children } = $props();
-</script>
-
-<div class="container-xl mx-auto main-content-1">
-	{@render children()}
-</div>
--- dist/date-utils.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Groups items by month and year
- * @param {Array<Object>} items - Array of data items
- * @param {string} [dateField='date'] - Field name in items that contains the date
- */
-export function groupByMonth(items: Array<Object>, dateField?: string): {};
-export function parseDate(...args: any[]): any;
