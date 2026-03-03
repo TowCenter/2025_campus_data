0a0317bf22eff1fea016bf00acbe85627a4156c1
Tory L <lysiktory@gmail.com>
2026-03-03 10:13:42 -0500

    Update +layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -5,7 +5,7 @@ export const prerender = true;
 export const load = async ({ fetch }) => {
 	try {
 		const metadata = await fetchTolerantJson(URLS.metadata, { fetch });
-		return { metadata };
+		return { metadata: { ...metadata, completed_at: '2026-03-02T00:00:00-05:00' } };
 	} catch {
 		return { metadata: null };
 	}
